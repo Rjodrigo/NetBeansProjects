3c4a7e74ff3fd8719bc783270f54c110d8302a96
Rodrigo Oliveira <144641235+Rjodrigo@users.noreply.github.com>
2023-12-18 10:20:25 -0300

    Add files via upload

---
+++ OperadoresAritimeticos/src/operadoresaritimeticos/OperadoresAritimeticos.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package operadoresaritimeticos;
+
+/**
+ *
+ * @author ogird
+ */
+public class OperadoresAritimeticos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        /*int n1 = 3;
+        int n2 = 5;
+        float m = (n1 + n2)/2; // A diferença do sinal de soma.: aqui ele soma porque esta dentro de uma operação marcada por parenteses        
+        System.out.println("A média é " + m);//E aqui ele esta concatenando ja que aqui se trata de uma "String"
+        
+        */
+        
+        /*int numero = 10;
+        int valor = 4 + numero--;// A sintaxe do ++ no Java é bem parecido com o conceito "if" e "else" do JavaScript
+        System.out.println(valor); //antes da variavel ele adiciona +1 se após o valor é negado
+        System.out.println(numero);
+        */
+        
+        /*int x = 4;
+        x *= 2;
+        System.out.println(x);
+        */
+        
+        /*float v = 8.5f;
+        int ar = (int) Math.round(v);
+        System.out.println(ar);
+        */
+        
+        double ale = Math.random();
+        int n = (int) (55 + ale * (50-15));
+        System.out.println(ale);
+    }   
+    
+}
